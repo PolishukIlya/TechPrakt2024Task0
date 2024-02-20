914ffa3b4b15313cefd9b5f667c7d3a40c064e23
PolishukIlya <ipz221_pia@student.ztu.edu.ua>
2024-02-20 19:53:21 +0200

    Created a console project to calculate the amount

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть перше число:");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введіть друге число:");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = number1 + number2;
+
+            Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює {sum}");
+
+            Console.WriteLine("Натисніть будь-яку клавішу для завершення...");
+            Console.ReadKey();
+        }
+    }
+}
